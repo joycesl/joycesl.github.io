75f0f88b3d3c0343cd94fcc96e4eba00392f3cdd
joycesl <120999211+joycesl@users.noreply.github.com>
2023-02-12 03:49:46 +1300

    Scroll to top button added
    Scroll to top button added. Adapted from w3 collective code.

--- joyceScript.js
+++ joyceScript.js
@@ -1,7 +1,28 @@
-/* TO DO:
+// Scroll to top button
+// Adapted from: https://w3collective.com/scroll-to-top-button-javascript/
 
-** Navigation
 
-** Contact form
-
-*/
+document.addEventListener('DOMContentLoaded', function () {
+  const scrollTop = function () {
+  const scrollBtn = document.createElement("button");
+  scrollBtn.innerHTML = "&uarr;";
+  scrollBtn.setAttribute("id", "scroll-btn");
+  document.body.appendChild(scrollBtn);
+  const scrollBtnDisplay = function () {
+  window.scrollY > window.innerHeight
+    ? scrollBtn.classList.add("show")
+    : scrollBtn.classList.remove("show");
+	};
+	window.addEventListener("scroll", scrollBtnDisplay);
+	const scrollWindow = function () {  
+		if (window.scrollY != 0) {
+			setTimeout(function () {
+			window.scrollTo(0, window.scrollY - 500);
+				scrollWindow();
+			}, 10);
+		}
+	};
+	scrollBtn.addEventListener("click", scrollWindow);
+	};
+	scrollTop();
+});
